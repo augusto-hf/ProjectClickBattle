431d8458923c6762308d537d65ba95ec5dd9be9c
Augusto Henrique Ferreira <51714609+augusto-hf@users.noreply.github.com>
2025-05-05 11:11:45 -0300

    asdasd

--- Resources/Module/SkillsInfoModule.lua
+++ Resources/Module/SkillsInfoModule.lua
@@ -1,30 +1,6 @@
 local skills = {
 }
 
-skills.weapon = { -- use as template
-upgrade_level = 0,
-upgrade_bonus = 0.30,
-starting_price = 0,
-price_bonus = 1.00,
-passive_damage = 1,
-active_damage = 2,
-active_type = "physical",
-active_cooldown = 2,
-active_is_dot = false,
-active_dot_time = 0,
-is_in_cooldown = false,
-level_text = "level_weapon",
-shop_icon = "shop_icon_weapon",
-price_text = "value_weapon",
-skill_icon = "skill_button_weapon",
-cooldown_icon = "skill_cooldown_weapon",
-icon_sprites = {
-	[5] = "weapon_level2",
-	[10] = "weapon_level3",
-	[15] = "weapon_level4"
-}
-}
-
 
 skills.warrior_soap = {
 upgrade_level = 0,
