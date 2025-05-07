b41b296e365aaac0aafef792ca247a7362795f58
Augusto Henrique Ferreira <51714609+augusto-hf@users.noreply.github.com>
2025-05-07 10:10:39 -0300

    asdasd

--- Resources/Module/ClickSkillsInfoModule.lua
+++ Resources/Module/ClickSkillsInfoModule.lua
@@ -1,37 +1,12 @@
 local click_skills = {
 }
 
-click_skills.template = {
-	upgrade_level = 0,
-	upgrade_bonus = 1.00,
-	upgrade_price = 1.00,
-	starting_price = 100,
-	price_bonus = 1.00,
-	damage_value = 3,
-	damage_type = "physical",
-	is_in_cooldown = false,
-	cooldown = 30,
-	duration = 15,
-	cooldown_icon_node = "skill_cooldown_",
-	skill_node = "click_skill_button_",
-	shop_node = "_upgrade_button"
-}
-
 click_skills.heavy_gloves = {
 	upgrade_level = 0,
 	upgrade_bonus = 1.00,
-	starting_price = 100,
-	price_bonus = 1.50,
 	damage_value = 3,
 	damage_type = "neutral",
-	is_in_cooldown = false,
-	cooldown = 30,
 	duration = 15,
-	cooldown_icon_node = "skill_cooldown_heavy_gloves",
-	skill_node = "click_skill_button_heavy_glove",
-	shop_node = "heavy_glove_upgrade_button",
-	level_text = "level_heavy_gloves",
-	price_text = "value_heavy_gloves",
 }
 
 click_skills.bolder = {
